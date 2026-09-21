617d5c9b4a9b2989195c299a0e1b4b78721141bb
Anderson Ricardo <ar4504842@gmail.com>
2026-09-21 11:03:30 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("calcular").addEventListener("click", function () {
-  let v1 = Number(document.getElementById("base").value);
-  let v2 = Number(document.getElementById("altura").value);
-  let soma = (v1 * v2) / 2;
-  document.getElementById("resultado").innerText = soma;
-});
